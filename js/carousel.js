7ee34840f9d6737ee2fa8fe3c0a2d62324c61f5a
Christopher Wright <cmwright33@gmail.com>
2013-07-24 23:11:52 -0400

    added the changes in opacity

--- js/carousel.js
+++ js/carousel.js
@@ -6,6 +6,34 @@ var previous = document.getElementById('previous');
 
 carousel.style.marginLeft = 0;
 
+
+next.onmouseover = function(event) {
+  console.log(event);
+ next.style.opacity = 0.2;
+};
+
+next.onmouseout = function(event) {
+  console.log(event);
+  next.style.opacity = 0.5;
+};
+
+
+previous.onmouseover = function(event) {
+  console.log(event);
+  previous.style.opacity = 0.2;
+};
+
+previous.onmouseout = function(event) {
+  console.log(event);
+  previous.style.opacity = 0.5;
+};
+
+
+
+
+
+
+next.addEventListener("click", next);
 // Create mouseoveer and mouseexit events for the buttons
 // to change their opacity from .5 to .2
 
